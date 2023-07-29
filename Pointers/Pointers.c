64c8c8dd582c41709d6c7dcfc8b378e60814fbe0
Fernando Crelick <fccrelick@protonmail.com>
2023-07-28 21:24:47 -0300

    Pointers, structs, typedef and passing by reference

---
+++ Pointers/Pointers.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+
+typedef size_t accumulateInt;
+
+typedef struct
+{
+	accumulateInt sum;
+	accumulateInt num;
+} accumulateData;
+
+void newAccumulateData(accumulateData* data)
+{
+	data->sum = 0;
+	data->num = 0;
+}
+
+void accumulate(accumulateData* data, accumulateInt value)
+{
+	data->sum += value;
+	data->num++;
+}
+
+accumulateInt accumulateResult(accumulateData* data)
+{
+	return data->sum / data->num;
+}
+
+int main()
+{
+	accumulateData data;
+	newAccumulateData(&data);
+
+	accumulate(&data, 10);
+	accumulate(&data, 20);
+
+	accumulateInt avg = accumulateResult(&data);
+	printf("The average is: %lld", avg);
+
+	return 0;
+}
