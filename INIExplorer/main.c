a06c3ccd08b5aa0f4e858a8be014e705914b7c83
Fernando Crelick <fccrelick@protonmail.com>
2023-08-06 14:21:10 -0300

    converted the INIParser to a static lib

---
+++ INIExplorer/main.c
@@ -0,0 +1,51 @@
+#include <INIParser.h>
+
+#include <stdio.h>
+#include <string.h>
+
+void iniDataReady1(const char* section, const char* key, const char* value)
+{
+	printf_s("[%s] \n%s = %s\n\n", section, key, value);
+}
+
+void iniDataReady2(const char* section, const char* key, const char* value)
+{
+	printf_s("%s\\%s is %s\n", section, key, value);
+}
+
+int main(int argc, char** argv)
+{
+	// Validate argument1 count
+	if (argc < 2 || argc > 4)
+	{
+		printf_s("usage: INIParser [PathToIniFile]  <OPTIONAL:Format(1/2)> <OPTIONAL:PathToLogFile>");
+		return -1;
+	}
+
+	// Check the format
+	ini_callback callback = iniDataReady1;
+	if (argc >= 3 && strcmp(argv[2], "2") == 0)
+	{
+		callback = iniDataReady2;
+	}
+
+	// Check logfile path
+	const char* logfile = NULL;
+	if (argc >= 4)
+	{
+		logfile = argv[2];
+	}
+
+	// Parse the ini file
+	ini_parseIniFromFile(argv[1], logfile, callback);
+
+
+	// HEX 0 to indicate end of the string
+	char edit[256];
+	edit[0] = 'L';
+	edit[1] = 'O';
+	edit[2] = 'L';
+	edit[3] = '\0';
+
+	printf(edit);
+}
